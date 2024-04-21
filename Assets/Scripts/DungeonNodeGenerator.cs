ac418067404172123539a802c75ec66a3bbfb874
Onur Çelik <celikonur988@gmail.com>
2024-04-21 20:59:21 +0300

    basic random dungeon created

--- Assets/Scripts/DungeonNodeGenerator.cs
+++ Assets/Scripts/DungeonNodeGenerator.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DungeonNodeGenerator : MonoBehaviour
 {
@@ -36,9 +38,53 @@ public class DungeonNodeGenerator : MonoBehaviour
         {
             nodeData.WritePosition();
         }
+        
+        CreateDungeon();
+    }
+
+    [SerializeField] private GameObject visualNode;
+    private Vector2Int NodePoint = Vector2Int.zero;
+
+    private HashSet<Vector2Int> dungeonNodePosList = new HashSet<Vector2Int>();
+
+    private int iterationCount = 100;
+    
+    private void CreateDungeon()
+    {
+        dungeonNodePosList.Add(NodePoint);
+        var nextNodePos = NodePoint;
+        Instantiate(visualNode, new Vector3(nextNodePos.x,nextNodePos.y,0),Quaternion.identity);
+        
+        
+        for (int i = 0; i < iterationCount; i++)
+        {
+            nextNodePos += GetRandomDirection();
+            dungeonNodePosList.Add(nextNodePos);
+            Instantiate(visualNode, new Vector3(nextNodePos.x,nextNodePos.y,0),Quaternion.identity);
+        }
+        
+        
     }
     
-    
+    private Vector2Int UpDirection = Vector2Int.up;
+    private Vector2Int DownDirection = Vector2Int.down;
+    private Vector2Int RightDirection = Vector2Int.right;
+    private Vector2Int LeftDirection = Vector2Int.left;
+
+    private List<Vector2Int> directionList = new List<Vector2Int>();
+    private Vector2Int GetRandomDirection()
+    {
+        directionList.Add(UpDirection);
+        directionList.Add(DownDirection);
+        directionList.Add(RightDirection);
+        directionList.Add(LeftDirection);
+        
+        var randomDirection = directionList[Random.Range(0, directionList.Count)];
+        directionList.Clear();
+        return randomDirection;
+    }
+
+
 }
 
 public class Grid
